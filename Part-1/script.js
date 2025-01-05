abaada9e4c4ee32a159692b50f0af7f19cfa6303
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-01-05 12:58:39 +0530

    update

--- Part-1/script.js
+++ Part-1/script.js
@@ -1,21 +1,21 @@
 gsap.to("#box", {
-    borderRadius: "50%",
-    x: 1200,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    backgroundColor: "yellow",
-    scale: 1.38
-})
+  borderRadius: "50%",
+  x: 1200,
+  duration: 2,
+  delay: 1,
+  rotate: 360,
+  backgroundColor: "yellow",
+  scale: 1.38,
+});
 
 gsap.to("#box2", {
-    x: 1200,
-    duration: 2,
-    delay: 1,
-})
+  x: 1200,
+  duration: 2,
+  delay: 1,
+});
 
 gsap.from("h1", {
-    color: "red",
-    duration: 2,
-    delay:1,
-})
+  color: "red",
+  duration: 2,
+  delay: 1,
+});
