394204fccd49ff5b63cc03063241df0bbba231d4
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-01-06 01:01:07 +0530

    add_items

--- Part-1/script.js
+++ Part-1/script.js
@@ -6,25 +6,48 @@ gsap.to("#box", {
   rotate: 360,
   backgroundColor: "yellow",
   scale: 1.38,
-  repeat:-1,
-  yoyo:true,
+  repeat: -1,
+  yoyo: true,
 });
 
 gsap.to("#box2", {
   x: 1200,
   duration: 2,
   delay: 1,
-  yoyo:true,
-  repeat:-1,
+  yoyo: true,
+  repeat: -1,
 });
 
-gsap.from("h1", {
-  color: "red",
+// gsap.from("h1", {
+//   color: "red",
+//   opacity: 0,
+//   duration: 2,
+//   y: 40,
+//   delay: 1,
+//   stagger: 0.3,
+//   yoyo: true,
+//   repeat: -1,
+// });
+
+var tl = gsap.timeline();
+
+tl.from("h2", {
+  y: -20,
   opacity: 0,
-  duration: 2,
-  y: 40,
-  delay: 1,
+  duration: 1,
+  delay: 0.5,
+});
+
+tl.from("h4", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
   stagger: 0.3,
-  yoyo:true,
-  repeat:-1,
+});
+
+tl.from("#text", {
+  y: 20,
+  opacity: 0,
+  duration: 0.5,
+  scale: 0.2,
 });
