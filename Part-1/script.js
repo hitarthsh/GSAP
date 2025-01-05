5c7ceb898a34d584d2048f0e66876a901d0a8e9d
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-01-05 23:51:56 +0530

    Update

--- Part-1/script.js
+++ Part-1/script.js
@@ -6,16 +6,25 @@ gsap.to("#box", {
   rotate: 360,
   backgroundColor: "yellow",
   scale: 1.38,
+  repeat:-1,
+  yoyo:true,
 });
 
 gsap.to("#box2", {
   x: 1200,
   duration: 2,
   delay: 1,
+  yoyo:true,
+  repeat:-1,
 });
 
 gsap.from("h1", {
   color: "red",
+  opacity: 0,
   duration: 2,
+  y: 40,
   delay: 1,
+  stagger: 0.3,
+  yoyo:true,
+  repeat:-1,
 });
