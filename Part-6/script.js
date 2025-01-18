d7d3026a0e1ef5a39d661a57f4392717946b1936
Hitarth Shah <90967985+hitarthsh@users.noreply.github.com>
2025-01-19 00:31:46 +0530

    Create Text Animation

--- Part-6/script.js
+++ Part-6/script.js
@@ -6,19 +6,46 @@ function breakTheText() {
 
     var clutter = ""
 
-    splittedText.forEach(function (elem) {
-        clutter += `<span>${elem}</span>` // name = name + name
-    })
+    // splittedText.forEach(function (elem) {
+    //     clutter += `<span>${elem}</span>` // name = name + name
+    // })
 
+    var splittedText = h1Text.split("")
+
+    var halfValue = splittedText.length / 2
+
+    splittedText.forEach(function (elem, idx) {
+        if (idx < halfValue) {
+            clutter += `<span class="a">${elem}</span>`
+        } else {
+            clutter += `<span class="b">${elem}</span>`
+        }
+    })
     h1.innerHTML = clutter
 }
 
 breakTheText()
 
-gsap.from("h1 span", {
-    y: 50,
-    opacity: 0,
-    duration: 0.8,
-    delay: 0.2,
+// gsap.from("h1 span", {
+//     y: 50,
+//     opacity: 0,
+//     duration: 0.8,
+//     delay: 0.2,
+//     stagger: -0.15,
+// })
+
+gsap.from("h1 .a", {
+    y: 80,
+    duration: 0.6,
+    delay: 0.5,
+    stagger: 0.15,
+    opacity: 0
+})
+
+gsap.from("h1 .b", {
+    y: 80,
+    duration: 0.6,
+    delay: 0.5,
     stagger: -0.15,
+    opacity: 0
 })
